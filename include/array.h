715962d0cd495b088a749d4fc98f4cc2a7e10463
cyyzero <cyyzero16@qq.com>
2018-09-27 21:46:17 +0800

    Change indents.

--- include/array.h
+++ include/array.h
@@ -9,267 +9,267 @@
 namespace cyyzero
 {
 
-    template<typename T, std::size_t N>
-    struct __array_traits
+template<typename T, std::size_t N>
+struct __array_traits
+{
+    using _type = T[N];
+
+    static constexpr T& _S_ref(const _type& t, std::size_t pos) noexcept
     {
-        using _type = T[N];
-
-        static constexpr T& _S_ref(const _type& t, std::size_t pos) noexcept
-        {
-            return const_cast<T&>(t[pos]);
-        }
-
-        static constexpr T* _S_ptr(const _type& t) noexcept
-        {
-            return const_cast<T*>(t);
-        }
-    };
-
-    template<typename T>
-    struct __array_traits<T, 0>
-    {
-        struct _type { };
-
-        static constexpr T& _S_ref(const _type&, std::size_t) noexcept
-        {
-            return *static_cast<T*>(nullptr);
-        }
-
-        static constexpr T* _S_ptr(const _type&) noexcept
-        {
-            return nullptr;
-        }
-    };
-
-    template<typename T, std::size_t N>
-    struct array
-    {
-        using value_type             = T;
-        using size_type              = std::size_t;
-        using difference_type        = std::ptrdiff_t;
-        using reference              = value_type&;
-        using const_reference        = const value_type&;
-        using pointer                = value_type*;
-        using const_pointer          = const value_type*;
-        using iterator               = value_type*;
-        using const_iterator         = const value_type*;
-        using reverse_iterator       = std::reverse_iterator<iterator>;
-        using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-
-        
-        using _array_type = __array_traits<T, N>;
-        typename _array_type::_type _M_elems;
-
-        // Element access
-        constexpr reference at(size_type pos)
-        {
-            if (pos >= N)
-                throw std::out_of_range("pos too large");
-            return _array_type::_S_ref(_M_elems, pos);
-        }
-
-        constexpr const_reference at(size_type pos) const
-        {
-            if (pos >= N)
-                throw std::out_of_range("pos too large");
-            return _array_type::_S_ref(_M_elems, pos);
-        }
-
-        constexpr reference operator[](size_type pos) noexcept
-        {
-            return _array_type::_S_ref(_M_elems, pos);
-        }
-
-        constexpr const_reference operator[](size_type pos) const noexcept
-        {
-            return _array_type::_S_ref(_M_elems, pos);
-        }
-
-        constexpr reference front()
-        {
-            return _array_type::_S_ref(_M_elems, 0);
-        }
-
-        constexpr const_reference front() const
-        {
-            return _array_type::_S_ref(_M_elems, 0);
-        }
-
-        constexpr reference back()
-        {
-            return N ? _array_type::_S_ref(_M_elems, N-1)
-                     : _array_type::_S_ref(_M_elems, 0);
-        }
-
-        constexpr const_reference back() const
-        {
-            return N ? _array_type::_S_ref(_M_elems, N-1)
-                     : _array_type::_S_ref(_M_elems, 0);
-        }
-
-        constexpr pointer data() noexcept
-        {
-            return _array_type::_S_ptr(_M_elems);
-        }
-
-        constexpr const_pointer data() const noexcept
-        {
-            return _array_type::_S_ptr(_M_elems);
-        }
-
-        // Iterate
-
-        constexpr iterator begin() noexcept
-        {
-            return static_cast<iterator>(data());
-        }
-
-        constexpr const_iterator begin() const noexcept
-        {
-            return static_cast<const_iterator>(data());
-        }
-
-        constexpr const_iterator cbegin() noexcept
-        {
-            return static_cast<const_iterator>(data());
-        }
-
-        constexpr iterator end() noexcept
-        {
-            return static_cast<iterator>(data() + N);
-        }
-
-        constexpr const_iterator end() const noexcept
-        {
-            return static_cast<const_iterator>(data() + N);
-        }
-
-        constexpr const_iterator cend() const noexcept
-        {
-            return static_cast<const_iterator>(data() + N);
-        }
-
-        constexpr reverse_iterator rbegin() noexcept
-        {
-            return reverse_iterator(end());
-        }
-
-        constexpr const_reverse_iterator rbegin() const noexcept
-        {
-            return const_reverse_iterator(end());
-        }
-
-        constexpr const_reverse_iterator crbegin() const noexcept
-        {
-            return const_reverse_iterator(end());
-        }
-
-        constexpr reverse_iterator rend() noexcept
-        {
-            return reverse_iterator(begin());
-        }
-
-        constexpr const_reverse_iterator rend() const noexcept
-        {
-            return const_reverse_iterator(begin());
-        }
-
-        constexpr const_reverse_iterator crend() const noexcept
-        {
-            return const_reverse_iterator(begin());
-        }
-
-        // Capacity
-
-        constexpr bool empty() const noexcept
-        {
-            return begin() == end();
-        }
-
-        size_type size() const noexcept
-        {
-            return N;
-        }
-
-        constexpr size_type max_size() const noexcept
-        {
-            return N;
-        }
-
-        // Operation
-        void fill(const T& value)
-        {
-            std::fill_n(begin(), N, value);
-        }
-
-        void swap(array& other) noexcept(noexcept(swap(std::declval<T&>(), std::declval<T&>())))
-        {
-            std::swap_ranges(begin(), end(), other.begin());
-        }
-    };
-
-    template<typename T, std::size_t N>
-    inline bool operator==(const array<T, N>& lhs, const array<T, N>& rhs)
-    {
-        return std::equal(lhs.begin(), lhs.end(), rhs.begin());
+        return const_cast<T&>(t[pos]);
     }
 
-    template<typename T, std::size_t N>
-    inline bool operator!=(const array<T, N>& lhs, const array<T, N>& rhs)
+    static constexpr T* _S_ptr(const _type& t) noexcept
     {
-        return !(lhs == rhs);
+        return const_cast<T*>(t);
+    }
+};
+
+template<typename T>
+struct __array_traits<T, 0>
+{
+    struct _type { };
+
+    static constexpr T& _S_ref(const _type&, std::size_t) noexcept
+    {
+        return *static_cast<T*>(nullptr);
     }
 
-    template<typename T, std::size_t N>
-    inline bool operator<(const array<T, N>& lhs, const array<T, N>& rhs)
+    static constexpr T* _S_ptr(const _type&) noexcept
     {
-        return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+        return nullptr;
+    }
+};
+
+template<typename T, std::size_t N>
+struct array
+{
+    using value_type             = T;
+    using size_type              = std::size_t;
+    using difference_type        = std::ptrdiff_t;
+    using reference              = value_type&;
+    using const_reference        = const value_type&;
+    using pointer                = value_type*;
+    using const_pointer          = const value_type*;
+    using iterator               = value_type*;
+    using const_iterator         = const value_type*;
+    using reverse_iterator       = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    
+    using _array_type = __array_traits<T, N>;
+    typename _array_type::_type _M_elems;
+
+    // Element access
+    constexpr reference at(size_type pos)
+    {
+        if (pos >= N)
+            throw std::out_of_range("pos too large");
+        return _array_type::_S_ref(_M_elems, pos);
     }
 
-    template<typename T, std::size_t N>
-    inline bool operator>(const array<T, N>& lhs, const array<T, N>& rhs)
+    constexpr const_reference at(size_type pos) const
     {
-        return rhs < lhs;
+        if (pos >= N)
+            throw std::out_of_range("pos too large");
+        return _array_type::_S_ref(_M_elems, pos);
     }
 
-    template<typename T, std::size_t N>
-    inline bool operator<=(const array<T, N>& lhs, const array<T, N>& rhs)
+    constexpr reference operator[](size_type pos) noexcept
     {
-        return !(lhs > rhs);
+        return _array_type::_S_ref(_M_elems, pos);
     }
 
-    template<typename T, std::size_t N>
-    inline bool operator>=(const array<T, N>& lhs, const array<T, N>& rhs)
+    constexpr const_reference operator[](size_type pos) const noexcept
     {
-        return !(lhs < rhs);
+        return _array_type::_S_ref(_M_elems, pos);
     }
 
-    template<std::size_t I, typename T, std::size_t N>
-    constexpr T& get(array<T, N>& a) noexcept
+    constexpr reference front()
     {
-        static_assert(I < N, "index is out of bound");
-        return a.at(I);
+        return _array_type::_S_ref(_M_elems, 0);
     }
 
-    template<std::size_t I, typename T, std::size_t N>
-    constexpr T&& get(array<T, N>&& a) noexcept
+    constexpr const_reference front() const
     {
-        return std::move(get<I>(a));
+        return _array_type::_S_ref(_M_elems, 0);
     }
 
-    template<std::size_t I, typename T, std::size_t N>
-    constexpr const T& get(const array<T, N>& a) noexcept
+    constexpr reference back()
     {
-        return static_cast<const T&>(
-            get<I>(const_cast<array<T, N>&>(a))
-        );
+        return N ? _array_type::_S_ref(_M_elems, N-1)
+                    : _array_type::_S_ref(_M_elems, 0);
     }
 
-    template<std::size_t I, typename T, std::size_t N>
-    constexpr const T&& get(const array<T, N>&& a) noexcept
+    constexpr const_reference back() const
     {
-        return std::remove(get<I>(a));
+        return N ? _array_type::_S_ref(_M_elems, N-1)
+                    : _array_type::_S_ref(_M_elems, 0);
     }
+
+    constexpr pointer data() noexcept
+    {
+        return _array_type::_S_ptr(_M_elems);
+    }
+
+    constexpr const_pointer data() const noexcept
+    {
+        return _array_type::_S_ptr(_M_elems);
+    }
+
+    // Iterate
+
+    constexpr iterator begin() noexcept
+    {
+        return static_cast<iterator>(data());
+    }
+
+    constexpr const_iterator begin() const noexcept
+    {
+        return static_cast<const_iterator>(data());
+    }
+
+    constexpr const_iterator cbegin() noexcept
+    {
+        return static_cast<const_iterator>(data());
+    }
+
+    constexpr iterator end() noexcept
+    {
+        return static_cast<iterator>(data() + N);
+    }
+
+    constexpr const_iterator end() const noexcept
+    {
+        return static_cast<const_iterator>(data() + N);
+    }
+
+    constexpr const_iterator cend() const noexcept
+    {
+        return static_cast<const_iterator>(data() + N);
+    }
+
+    constexpr reverse_iterator rbegin() noexcept
+    {
+        return reverse_iterator(end());
+    }
+
+    constexpr const_reverse_iterator rbegin() const noexcept
+    {
+        return const_reverse_iterator(end());
+    }
+
+    constexpr const_reverse_iterator crbegin() const noexcept
+    {
+        return const_reverse_iterator(end());
+    }
+
+    constexpr reverse_iterator rend() noexcept
+    {
+        return reverse_iterator(begin());
+    }
+
+    constexpr const_reverse_iterator rend() const noexcept
+    {
+        return const_reverse_iterator(begin());
+    }
+
+    constexpr const_reverse_iterator crend() const noexcept
+    {
+        return const_reverse_iterator(begin());
+    }
+
+    // Capacity
+
+    constexpr bool empty() const noexcept
+    {
+        return begin() == end();
+    }
+
+    size_type size() const noexcept
+    {
+        return N;
+    }
+
+    constexpr size_type max_size() const noexcept
+    {
+        return N;
+    }
+
+    // Operation
+    void fill(const T& value)
+    {
+        std::fill_n(begin(), N, value);
+    }
+
+    void swap(array& other) noexcept(noexcept(swap(std::declval<T&>(), std::declval<T&>())))
+    {
+        std::swap_ranges(begin(), end(), other.begin());
+    }
+};
+
+template<typename T, std::size_t N>
+inline bool operator==(const array<T, N>& lhs, const array<T, N>& rhs)
+{
+    return std::equal(lhs.begin(), lhs.end(), rhs.begin());
+}
+
+template<typename T, std::size_t N>
+inline bool operator!=(const array<T, N>& lhs, const array<T, N>& rhs)
+{
+    return !(lhs == rhs);
+}
+
+template<typename T, std::size_t N>
+inline bool operator<(const array<T, N>& lhs, const array<T, N>& rhs)
+{
+    return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+}
+
+template<typename T, std::size_t N>
+inline bool operator>(const array<T, N>& lhs, const array<T, N>& rhs)
+{
+    return rhs < lhs;
+}
+
+template<typename T, std::size_t N>
+inline bool operator<=(const array<T, N>& lhs, const array<T, N>& rhs)
+{
+    return !(lhs > rhs);
+}
+
+template<typename T, std::size_t N>
+inline bool operator>=(const array<T, N>& lhs, const array<T, N>& rhs)
+{
+    return !(lhs < rhs);
+}
+
+template<std::size_t I, typename T, std::size_t N>
+constexpr T& get(array<T, N>& a) noexcept
+{
+    static_assert(I < N, "index is out of bound");
+    return a.at(I);
+}
+
+template<std::size_t I, typename T, std::size_t N>
+constexpr T&& get(array<T, N>&& a) noexcept
+{
+    return std::move(get<I>(a));
+}
+
+template<std::size_t I, typename T, std::size_t N>
+constexpr const T& get(const array<T, N>& a) noexcept
+{
+    return static_cast<const T&>(
+        get<I>(const_cast<array<T, N>&>(a))
+    );
+}
+
+template<std::size_t I, typename T, std::size_t N>
+constexpr const T&& get(const array<T, N>&& a) noexcept
+{
+    return std::remove(get<I>(a));
+}
 
 }
 #endif
--- include/defaulloc.h
+++ include/defaulloc.h
@@ -9,107 +9,107 @@
 
 namespace cyyzero
 {
-    template <typename T>
-    class allocator
+template <typename T>
+class allocator
+{
+public:
+    using value_type      = T;
+    using pointer         = T*;
+    using const_pointer   = const T*;
+    using reference       = T&;
+    using const_reference = const T&;
+    using size_type       = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using propagate_on_container_move_assignment = std::true_type;
+    // constructors
+    allocator() noexcept { }
+
+    allocator(const allocator&) noexcept { }
+
+    template<typename P>
+    allocator(const allocator<P>&) noexcept { }
+
+    // destructor
+    ~allocator()  noexcept { }
+
+    template<typename T1>
+    struct rebind
     {
-    public:
-        using value_type      = T;
-        using pointer         = T*;
-        using const_pointer   = const T*;
-        using reference       = T&;
-        using const_reference = const T&;
-        using size_type       = std::size_t;
-        using difference_type = std::ptrdiff_t;
-        using propagate_on_container_move_assignment = std::true_type;
-        // constructors
-        allocator() noexcept { }
-
-        allocator(const allocator&) noexcept { }
-
-        template<typename P>
-        allocator(const allocator<P>&) noexcept { }
-
-        // destructor
-        ~allocator()  noexcept { }
-
-        template<typename T1>
-        struct rebind
-        {
-            using other = allocator<T1>;
-        };
-
-        pointer allocate(size_type n, void *p = 0)
-        {
-            if (n > this->max_size())
-            {
-                throw std::bad_alloc();
-            }
-            return static_cast<pointer>(::operator new(n * sizeof(T)));
-        }
-
-        void deallocate(pointer p, size_type)
-        {
-            ::operator delete(p);
-        }
-
-        pointer address(reference x) const noexcept
-        {
-            return std::addressof(x);
-        }
-
-        const_pointer address(const_reference x) const noexcept
-        {
-            return std::addressof(x);
-        }
-
-        size_type max_size() const
-        {
-            return std::size_t(-1) / sizeof(T);
-        }
-
-        template<typename U, typename... Args>
-        void construct(U* p, Args&&... args) const
-        {
-            ::new((void*)p) U(std::forward<Args>(args)...);
-        }
-
-        template<typename U>
-        void destroy(U* p)
-        {
-            p->~U();
-        }
+        using other = allocator<T1>;
     };
 
-    template<typename T>
-    inline bool operator==(const allocator<T>&, const allocator<T>&) noexcept
+    pointer allocate(size_type n, void *p = 0)
     {
-        return true;
+        if (n > this->max_size())
+        {
+            throw std::bad_alloc();
+        }
+        return static_cast<pointer>(::operator new(n * sizeof(T)));
     }
 
-    template<typename T1, typename T2>
-    inline bool operator==(const allocator<T1>&, const allocator<T2>&) noexcept
+    void deallocate(pointer p, size_type)
     {
-        return true;
+        ::operator delete(p);
     }
 
-    template<typename T>
-    inline bool operator!=(const allocator<T>&, const allocator<T>&) noexcept
+    pointer address(reference x) const noexcept
     {
-        return false;
+        return std::addressof(x);
     }
 
-    template<typename T1, typename T2>
-    inline bool operator!=(const allocator<T1>&, const allocator<T2>&) noexcept
+    const_pointer address(const_reference x) const noexcept
     {
-        return false;
+        return std::addressof(x);
     }
 
-    template<>
-    class allocator<void>
+    size_type max_size() const
     {
-    public:
-        using pointer = void*;
-    };
+        return std::size_t(-1) / sizeof(T);
+    }
+
+    template<typename U, typename... Args>
+    void construct(U* p, Args&&... args) const
+    {
+        ::new((void*)p) U(std::forward<Args>(args)...);
+    }
+
+    template<typename U>
+    void destroy(U* p)
+    {
+        p->~U();
+    }
+};
+
+template<typename T>
+inline bool operator==(const allocator<T>&, const allocator<T>&) noexcept
+{
+    return true;
+}
+
+template<typename T1, typename T2>
+inline bool operator==(const allocator<T1>&, const allocator<T2>&) noexcept
+{
+    return true;
+}
+
+template<typename T>
+inline bool operator!=(const allocator<T>&, const allocator<T>&) noexcept
+{
+    return false;
+}
+
+template<typename T1, typename T2>
+inline bool operator!=(const allocator<T1>&, const allocator<T2>&) noexcept
+{
+    return false;
+}
+
+template<>
+class allocator<void>
+{
+public:
+    using pointer = void*;
+};
 }
 
 #endif
