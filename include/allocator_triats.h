de9ab12d844a2ff11ccffdeac76e806754c2e6c5
cyyzero <cyyzero16@qq.com>
2018-11-20 23:45:02 +0800

    update :use cyy::pointer_traits in allocator_traits

--- include/allocator_triats.h
+++ include/allocator_triats.h
@@ -4,6 +4,7 @@
 #include <type_traits>
 #include <utility>
 #include <limits>
+#include "pointer_traits.h"
 
 namespace cyy
 {
@@ -50,7 +51,7 @@ private:                                                     \
                                                              \
     static T2 T1##test(...);                                 \
                                                              \
-    using _##T1 = decltype(T1##test(nullptr));                   \
+    using _##T1 = decltype(T1##test(nullptr));               \
 public:                                                      \
 
 // pointer: Alloc::pointer if present, otherwise value_type*
@@ -58,20 +59,20 @@ ALIAS_USING_SFINAE(pointer, value_type*)
     using pointer = _pointer;
 
 // const_pointer: Alloc::const_pointer if present,
-//                otherwise std::std::pointer_traits<pointer>::rebind<const value_type>
-ALIAS_USING_SFINAE(const_pointer, typename std::pointer_traits<pointer>::template rebind<const value_type>)
+//                otherwise std::cyy::pointer_traits<pointer>::rebind<const value_type>
+ALIAS_USING_SFINAE(const_pointer, typename cyy::pointer_traits<pointer>::template rebind<const value_type>)
     using const_pointer = _const_pointer;
 
-// void_pointer: Alloc::void_pointer if present, otherwise std::std::pointer_traits<pointer>::rebind<void>
-ALIAS_USING_SFINAE(void_pointer, typename std::pointer_traits<pointer>::template rebind<void>)
+// void_pointer: Alloc::void_pointer if present, otherwise std::cyy::pointer_traits<pointer>::rebind<void>
+ALIAS_USING_SFINAE(void_pointer, typename cyy::pointer_traits<pointer>::template rebind<void>)
     using void_pointer = _void_pointer;
 
-// const_void_pointer: Alloc::const_void_pointer if present, otherwise std::std::pointer_traits<pointer>::rebind<const void>
-ALIAS_USING_SFINAE(const_void_pointer, typename std::pointer_traits<pointer>::template rebind<const void>)
+// const_void_pointer: Alloc::const_void_pointer if present, otherwise std::cyy::pointer_traits<pointer>::rebind<const void>
+ALIAS_USING_SFINAE(const_void_pointer, typename cyy::pointer_traits<pointer>::template rebind<const void>)
     using const_void_pointer = _const_void_pointer;
 
-// different_type: Alloc::difference_type if present, otherwise std::std::pointer_traits<pointer>::difference_type
-ALIAS_USING_SFINAE(difference_type, typename std::pointer_traits<pointer>::difference_type)
+// different_type: Alloc::difference_type if present, otherwise std::cyy::pointer_traits<pointer>::difference_type
+ALIAS_USING_SFINAE(difference_type, typename cyy::pointer_traits<pointer>::difference_type)
     using difference_type = _difference_type;
 
 // size_type: Alloc::size_type if present, otherwise std::make_unsigned<difference_type>::type
