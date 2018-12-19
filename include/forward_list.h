903c57f1cbdd13df54f9d4253cc1b84e0452e973
cyyzero <cyyzero16@qq.com>
2018-12-19 23:37:57 +0800

    Add  class `Forward_list`.

---
+++ include/forward_list.h
@@ -0,0 +1,47 @@
+#ifndef FORWARD_LIST_H
+#define FORWARD_LIST_H
+
+#include "allocator.h"
+#include "allocator_traits.h"
+
+namespace cyy
+{
+namespace detail
+{
+// base class for Forward_list
+template<typename T, typename Allocator>
+struct Fwd_list_base
+{
+    using Alloc_traits = cyy::allocator_traits<Allocator>;
+    // using   cyy::allocator_traits<Allocator>::rebind_alloc<>;
+
+
+}; // class Fwd_list
+} // namespace detail
+
+template<typename T, typename Allocator = allocator<T>>
+class Forward_list
+{
+private:
+    using Base             = Fwd_list_base<T, Allocator>;
+    using Node             = Fwd_list_node<T>;
+    using Node_base        = Fwd_list_node_base;
+    using Alloc_traits     = cyy::allocator_traits<Allocator>;
+
+public:
+    using value_type      = T;
+    using allocator_type  = Allocator;
+    using size_type       = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference       = T&;
+    using const_reference = const T&;
+    using pointer         = typename allocator_traits<Allocator>::pointer;
+    using const_pointer   = typename allocator_traits<Allocator>::const_pointer;
+    using iterator        = ;
+    using const_iterator  = ;
+
+}; // class Forward_list
+
+} // namespace cyy
+
+#endif // FORWARD_LIST_H
