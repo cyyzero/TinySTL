02c884565848058e2beeb59845d971e783682b37
cyyzero <cyyzero16@qq.com>
2019-08-05 02:47:57 +0800

    Add tests for List constructors

--- tests/list_example.cpp
+++ tests/list_example.cpp
@@ -1,9 +1,22 @@
 #include "list.h"
 
+#include <string>
 #include <iostream>
+#include <cassert>
 
 using namespace cyy;
 using namespace std;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& s, const List<T>& v) {
+    s.put('[');
+    char comma[3] = {'\0', ' ', '\0'};
+    for (const auto& e : v) {
+        s << comma << e;
+        comma[0] = ',';
+    }
+    return s << ']';
+}
 
 int main()
 {
@@ -16,5 +29,27 @@ int main()
                   << (it1 == it1) << (it1 != it1)
                   << (it2 == it2) << (it2 != it2)
                   << (it2 == it1) << (it2 != it1) << "\n";
+    }
+    
+    std::cout << "\nTest for constructors\n";
+    {
+        // c++11 initializer list syntax:
+        List<std::string> words1 {"the", "frogurt", "is", "also", "cursed"};
+        std::cout << "words1: " << words1 << '\n';
+    
+        // words2 == words1
+        List<std::string> words2(words1.begin(), words1.end());
+        // assert(words1 == words2);
+        std::cout << "words2: " << words2 << '\n';
+    
+        // words3 == words1
+        List<std::string> words3(words1);
+        // assert(words1 == words3);
+        std::cout << "words3: " << words3 << '\n';
+    
+        // words4 is {"Mo", "Mo", "Mo", "Mo", "Mo"}
+        List<std::string> words4(5, "Mo");
+        // assert(words4 == List<std::string>{"Mo", "Mo", "Mo", "Mo", "Mo"});
+        std::cout << "words4: " << words4 << '\n';
     }
 }
